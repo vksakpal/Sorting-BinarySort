8fd1363f1d22c2b2688079cf19533871a058849d
vksakpal <vinay.sakpal@gmail.com>
2022-12-19 09:00:13 +0530

    Add project files.

---
+++ BinarySort/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("What is the max number");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+
+List<int> numbers = new List<int>();
+Random random = new Random();
+
+for (int i = 1 ; i <= maxNumber;i++)
+{
+    var number = random.Next(1, maxNumber);
+    numbers.Add(number);
+}
+
+for(int i = 1 ; i <= numbers.Count ;i ++)
+{
+    Console.WriteLine(numbers[i-1]);
+}
+
+Console.ReadLine();
+
